c6080afc8fb294704ab31a0f8035d51afc411eec
steve <steve.ye.developer@gmail.com>
2020-10-18 00:11:30 +0900

    Add rest api read me and project

---
+++ 03-rest-api/src/main/java/com/fastcampus/java/model/SearchParam.java
@@ -0,0 +1,32 @@
+package com.fastcampus.java.model;
+
+public class SearchParam {
+
+    private String account;
+    private String email;
+    private int page;
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+}
